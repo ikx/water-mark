9e7ed27ffb36186215ebb7def0c021e6869d4bff
rainy <rainy.sun@fu101.com>
2012-08-16 16:43:09 +0800

    split show_big_image to shoBigImg.h -> show()

--- src/show_big_image.c
+++ src/show_big_image.c
@@ -1,12 +1,4 @@
-#include "highgui.h"
-#include "cv.h"
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-#define MAXWIDTH 1000
-#define MAXHEIGHT 700
-
+#include "lib/showBigImg.h"
 int main(int argc, char** argv){
 	int imgw, imgh;
 	int windows;
@@ -21,28 +13,6 @@ int main(int argc, char** argv){
 		printf("Failed to load image \"%s\"\n", argv[1]);
 		exit(1);
 	}
-	imgw = img->width;
-	imgh = img->height;
-	printf("image height: %d\n", imgh);
-	windows = ceil((double)imgh / MAXHEIGHT );
-	printf("windows: %d\n", windows);
-
-	char winame[windows][7];
-	for(i=0;i<windows;i++){
-		sprintf(winame[i], "part%d", i+1);
-		CvPoint roi_lt = cvPoint(0, MAXHEIGHT * i);
-		int roi_h      = (i != windows - 1) ? MAXHEIGHT : (MAXHEIGHT * windows - imgh);
-		int roi_w	   = imgw;
-		CvRect roi     = cvRect(roi_lt.x, roi_lt.y, roi_w, roi_h);
-		cvSetImageROI(img, roi);
-		cvNamedWindow(winame[i], CV_WINDOW_AUTOSIZE);
-		cvShowImage(winame[i],img);
-	}
-	cvWaitKey(0);
-	for(i=0;i<windows;i++){
-		printf("windows: %s\n", winame[i]);
-		cvDestroyWindow(winame[i]);
-	}
-	cvResetImageROI(img);
+	show(img);
 	cvReleaseImage(&img);
 }
